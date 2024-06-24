8f49ddee82892c8bcbc56f885f889f70064dfcbf
Richie Chenyuqi <richie.chen.yu.qi@gmail.com>
2024-06-24 13:58:01 +0800

    See description
    Manually uploaded without pushing (not working) via XCode. Bug fixes with SQLite established and sample porting with mock server example.

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 Use algorithm/logic only. UI not tested
 Saved data are only done locally - no sql db set up or portal for linking servers
-#TODO: Add SQLite similar to my implementation in Cotlin
--- data_entry/Entry.swift
+++ data_entry/Entry.swift
@@ -8,8 +8,15 @@
 import Foundation
 
 struct Entry: Identifiable {
-    let id = UUID()
+    let id: UUID  // Using UUID to ensure each entry has a unique identifier
     var userName: String
     var items: [(description: String, value: String)]
     var timestamp: String
+
+    init(userName: String, items: [(description: String, value: String)], timestamp: String) {
+        self.id = UUID()  // Automatically generate a new UUID
+        self.userName = userName
+        self.items = items
+        self.timestamp = timestamp
+    }
 }
